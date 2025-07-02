0bc75fb7b42f869d157ca8faa46f1787af1bd332
Sebastia <calderonsebastian211@gmail.com>
2025-07-02 09:54:59 -0500

    update: ejm7 (v.1.1)

--- ejemplo7/proyectouno/administrativo/forms.py
+++ ejemplo7/proyectouno/administrativo/forms.py
@@ -4,17 +4,33 @@ from administrativo.models import Estudiante, Modulo, Matricula
 class EstudianteForm(forms.ModelForm):
     class Meta:
         model = Estudiante
-        fields = ['nombre', 'apellido', 'cedula', 'edad', 'tipo_estudiante']
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'cedula': forms.TextInput(attrs={'class': 'form-control'}),
+            'edad': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tipo_estudiante': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 class ModuloForm(forms.ModelForm):
     class Meta:
         model = Modulo
-        fields = ['nombre']
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.Select(attrs={'class': 'form-control'}),
+        }   
 
 class MatriculaForm(forms.ModelForm):
     class Meta:
         model = Matricula
-        fields = ['estudiante', 'modulo', 'comentario', 'costo']
+        fields = '__all__'
+        widgets = {
+            'estudiante': forms.Select(attrs={'class': 'form-control'}),
+            'modulo': forms.Select(attrs={'class': 'form-control'}),
+            'comentario': forms.TextInput(attrs={'class': 'form-control'}),
+            'costo': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
 class MatriculaEditForm(forms.ModelForm):
     class Meta:
