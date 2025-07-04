42ee74ec269f052366690f7570beaad6db925fcb
Sebastia <calderonsebastian211@gmail.com>
2025-07-04 18:35:09 -0500

    update: ejm1(v.1.3)

--- ejemplo7/proyectouno/administrativo/forms.py
+++ ejemplo7/proyectouno/administrativo/forms.py
@@ -16,7 +16,8 @@ class EstudianteForm(forms.ModelForm):
 class ModuloForm(forms.ModelForm):
     class Meta:
         model = Modulo
-        fields = '__all__'
+        fields = ['nombre']
+
         widgets = {
             'nombre': forms.Select(attrs={'class': 'form-control'}),
         }   
@@ -36,3 +37,9 @@ class MatriculaEditForm(forms.ModelForm):
     class Meta:
         model = Matricula
         fields = ['estudiante', 'modulo', 'comentario', 'costo']
+        widgets = {
+            'estudiante': forms.Select(attrs={'class': 'form-control'}),
+            'modulo': forms.Select(attrs={'class': 'form-control'}),
+            'comentario': forms.TextInput(attrs={'class': 'form-control'}),
+            'costo': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
